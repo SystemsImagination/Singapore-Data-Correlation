cd5b2e0e573baed644bf19b6f66722e6cf40cb96
sarahf5676 <39970608+sarahf5676@users.noreply.github.com>
2018-06-20 13:31:12 -0700

    Found probability of c in b in a

--- correlatedata.py
+++ correlatedata.py
@@ -1,18 +1,20 @@
 import pandas as pd
 df = pd.read_excel("C:\\Users\\Groot\\Documents\\all data.xlsx", sheet = 1)
 df.fillna(0, inplace=True)
-df.replace("NA", 0)
+
 
 def drop_x_row(df, x, column):
      df_true_false_values = df[df[column] == x]
      df = df.drop(df_true_false_values.index, axis=0)
      return df
+"""
 df = drop_x_row(df, 0,"family_mother_smoke_yes")
 df = drop_x_row(df, 0, "family_father_smoke_yes")
 df = drop_x_row(df, "No", "family_mother_smoke_yes")
 df = drop_x_row(df, "No", "family_father_smoke_yes")
 print(df)
 arr = list(df)
+"""
 """
 for i in range(51,143):
 	for j in range(51, 143):
@@ -22,6 +24,35 @@ for i in range(51,143):
 			print(corr)
 """
 
+count1 = 0
+for i in range(0, len(df.axes[0])):
+	arr = list(df)
+	if df.ix[i, arr.index("allergic_rhinitis_any_nose_problem")] == "Yes":
+		if df.ix[i, arr.index("allergic_rhinitis_itchy_eyes")] == "Yes":
+			if df.ix[i, arr.index("allergic_rhinitis_itchy_nose_intermittent")] == "Yes":
+				count1 = count1 + 1
+print(count1)
+prob_of_b_in_a = count1/(len(df.axes[0]) * 3)
+print(prob_of_b_in_a)
 def add_two_columns_together(df, col1, col2):
 	df["sum"] = df[col1] + df[col2]
 	return df
+
+"""
+def probability_of_b_including_a(df, a, b, col1, col2):
+	arr = list(df)
+	a_count = 0
+	b_count = 0
+	loc_of_col1 = arr.index(col1)
+	loc_of_col2 = arr.index(col2)
+	for i in range(0, len(df.index)):
+		if df.ix[i, loc_of_col1] == b:
+			b_count = b_count + 1
+	for i in range(0, len(df.index)):
+		if df.ix[i, loc_of_col2] == a:
+			a_count = a_count + 1
+	prob_of_b_and_a = (a_count/len(df.axes[0])) * (b_count/len(df.axes[0]))
+	return prob_of_b_and_a
+	print(probability_of_b_including_a(df, "Yes", "Yes", "allergic_rhinitis_nose_bleed_no", "allergic_rhinitis_nose_bleed_persistent"))
+#def probability(df, col1, col2):
+"""
