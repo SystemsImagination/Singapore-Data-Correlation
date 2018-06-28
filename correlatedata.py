a696769e6b52d87eeaa2dc4ec9de444b2f64154d
sarahf5676 <39970608+sarahf5676@users.noreply.github.com>
2018-06-28 16:37:31 -0700

    4 way probability

--- correlatedata.py
+++ correlatedata.py
@@ -8,15 +8,14 @@ def drop_x_row(df, x, column):
      df_true_false_values = df[df[column] == x]
      df = df.drop(df_true_false_values.index, axis=0)
      return df
-
+def drop_row_by_index(df, row_index):
+	arr = list(df)
+	df = df.drop(row_index, axis = 0)
+	return df
 count1 = 0
 count2 = 0
 count3 = 0
 count4 = 0
-
-
-
-
 
 def column_to_list(df, col):
 	array = list(df)
@@ -38,7 +37,8 @@ def sort_column(df,col): #bubble sort algorithm
 
 list1 = sort_column(df, "[MIP-1b]")
 list2 = sort_column(df, "[MIP-1a]")
-
+#print(len(list1))
+#print(len(list2))
 
 def drop_middle_25(arr):
 	upper_bound = int(0.75*len(arr) - 1)
@@ -49,12 +49,44 @@ def drop_middle_25(arr):
 	return arr1
 list1 = drop_middle_25(list1)
 list2 = drop_middle_25(list2)
+middle_indeces1 = []
+middle_indeces2 = []
 df1 = df
 df3 = df
-for i in range(0, len(list2)):
-	df3 = drop_x_row(df3, list2[i], "[MIP-1a]")
-for i in range(0, len(list1)):
-	df1 = drop_x_row(df1, list1[i], "[MIP-1b]")
+arr2 = list(df)
+indeces = list(df.index.values)
+for i in range(0, int(len(df.axes[0])/2)):
+	for j in range(0, len(list1)):
+		if df.ix[indeces[i], arr2.index("[MIP-1b]")] == list1[j]:
+			middle_indeces1.append(indeces[i])
+for i in range(0, int(len(df.axes[0])/2)):
+	for j in range(0, len(list2)):
+		if df.ix[indeces[i], arr2.index("[MIP-1a]")] == list2[j]:
+			middle_indeces2.append(indeces[i])
+duplicates1 = []
+duplicates2 = []
+for i in range(0,len(middle_indeces1) - 1):
+	if middle_indeces1[i] == middle_indeces1[i + 1]:
+		duplicates1.append(middle_indeces1[i])
+for item in duplicates1:
+	middle_indeces1.remove(item)
+for i in range(0,len(middle_indeces2) - 1):
+	if middle_indeces2[i] == middle_indeces2[i + 1]:
+		duplicates2.append(middle_indeces2[i])
+for item in duplicates2:
+	middle_indeces2.remove(item)
+#print(middle_indeces1)
+#print(middle_indeces2)
+
+df1 = df
+df3 = df
+for item in middle_indeces2:
+	df3 = drop_row_by_index(df3, item)
+print(df3["[MIP-1a]"])
+for item in middle_indeces1:
+	df1 = drop_row_by_index(df1, item)
+print(df1["[MIP-1b]"])
+
 
 
 df1.sort_values(["[MIP-1a]"], ascending = True, inplace = True)
@@ -64,6 +96,7 @@ print(df1["[MIP-1a]"])
 print(df3["[MIP-1b]"])
 indeces1 = list(df1.index.values)
 indeces2 = list(df3.index.values)
+
 value_index = []
 for i in range(0, len(indeces2)):
 	for j in range(0, len(indeces1)):
@@ -73,41 +106,45 @@ for i in range(0, len(indeces2)):
 for i in range(0, len(value_index)):
 	arr = list(df)
 
-
-
+"""
 for i in range(int(len(value_index)/2), int(len(value_index))):
 	arr = list(df)
 	if df.ix[value_index[i], arr.index("mother_asthma")] == "Yes": #a
 		if df.ix[value_index[i], arr.index("father_asthma")] == "Yes": #b
 			count1 = count1 + 1
-percentage_a_b = (count1/(len(value_index) * 2))*100
+percentage_a_b = (count1/len(indeces1))*100
 print("A to B")
+
 print(percentage_a_b)
+
 for i in range(int(len(value_index)/2), int(len(value_index))):
 	if df.ix[value_index[i], arr.index("asthma_diagnosed")] == "Yes": #c
 		if df.ix[value_index[i], arr.index("mother_asthma")] == "Yes": #a
 			count2 = count2 + 1
-percentage_a_c = (count2/(len(value_index) * 2))*100
+percentage_a_c = (count2/len(value_index))*100
 print("A to C")
 print(percentage_a_c)
+
 for i in range(int(len(value_index)/2), int(len(value_index))):
 	arr = list(df)
 	if df.ix[value_index[i], arr.index("asthma_diagnosed")] == "Yes": #c
 		if df.ix[value_index[i], arr.index("father_asthma")] == "Yes": #b
 			count3 = count3 + 1
-percentage_b_c = (count3/(len(value_index) * 2))*100
+percentage_b_c = (count3/len(value_index))*100
 print("B to C")
 print(percentage_b_c)
+"""
+for i in range(int(len(value_index)/2), ):
+	if df.ix[value_index[i], arr.index("diet_burgers_fast_food")] == "Most or all days": #a
+		if df.ix[value_index[i], arr.index("allergic_rhinitis_snore_no")] == "Yes": #b
+			if df.ix[value_index[i], arr.index("asthma_after_exercise")] == "Yes": #c
+				if df.ix[value_index[i], arr.index("asthma_limit_speech")] == "Yes":
+					count4 = count4 + 1
 
-for i in range(int(len(value_index)/2), int(len(value_index))):
-	if df.ix[value_index[i], arr.index("mother_asthma")] == "Yes": #a
-		if df.ix[value_index[i], arr.index("father_asthma")] == "Yes": #b
-			if df.ix[value_index[i], arr.index("asthma_diagnosed")] == "Yes": #c
-				count4 = count4 + 1
-percentage_a_b_c = (count4/(len(value_index) * 3))*100
-print("A to B to C")
-print(percentage_a_b_c)
-
+percentage_a_b_c_d = (count4/(len(value_index))* 100)
+print("A to B to C to D")
+print(percentage_a_b_c_d)
+#4 way probability
 
 
 
